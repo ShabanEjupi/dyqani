7b08ccb6b9fe2d168b747fb657c2c9a82d784298
QabiEj <shaban.ejupi@student.uni-pr.edu>
2025-04-16 22:51:59 +0200

    Ndryshime tjera

--- js/instagram-image-cache.js
+++ js/instagram-image-cache.js
@@ -5,14 +5,14 @@
 
 // Cached versions of the Instagram images (these won't expire)
 const instagramImageCache = {
-    'https://www.instagram.com/p/DC9njmdIeNT/': 'https://i.imgur.com/v9YYyPP.jpg',
-    'https://www.instagram.com/p/DC9oeeXsIxD/': 'https://i.imgur.com/QTFCNuq.jpg',
-    'https://www.instagram.com/p/DDrbMUTohRm/': 'https://i.imgur.com/8bvSnXk.jpg',
-    'https://www.instagram.com/p/DE2H4BGqEy9/': 'https://i.imgur.com/h5rQ0NF.jpg',
-    'https://www.instagram.com/p/DE2IdJ5qub5/': 'https://i.imgur.com/PCmnb80.jpg',
-    'https://www.instagram.com/p/DE2Jy0NKbLi/': 'https://i.imgur.com/LZfCVvn.jpg',
-    'https://www.instagram.com/p/DE2KF1RK7yK/': 'https://i.imgur.com/yJpXeuo.jpg',
-    'https://www.instagram.com/p/DC9khPuIaql/': 'https://i.imgur.com/j4YoS9K.jpg'
+    'https://www.instagram.com/p/DC9njmdIeNT/': '../assets/icons/product1.png',
+    'https://www.instagram.com/p/DC9oeeXsIxD/': '../assets/icons/product2.png',
+    'https://www.instagram.com/p/DDrbMUTohRm/': '../assets/icons/product3.png',
+    'https://www.instagram.com/p/DE2H4BGqEy9/': '../assets/icons/product4.png',
+    'https://www.instagram.com/p/DE2IdJ5qub5/': '../assets/icons/product5.png',
+    'https://www.instagram.com/p/DE2Jy0NKbLi/': '../assets/icons/product6.png',
+    'https://www.instagram.com/p/DE2KF1RK7yK/': '../assets/icons/product7.png',
+    'https://www.instagram.com/p/DC9khPuIaql/': '../assets/icons/product8.png'
 };
 
 /**
