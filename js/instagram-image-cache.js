58d6b352c572513f6ca74e96c9ac3ca4cc05d6ba
QabiEj <shaban.ejupi@student.uni-pr.edu>
2025-04-16 22:57:03 +0200

    Ndryshime tjera

--- js/instagram-image-cache.js
+++ js/instagram-image-cache.js
@@ -3,24 +3,15 @@
  * This script helps load Instagram images by providing permanent URLs
  */
 
-// Detect if we are on the main page or subpage
-const isMainPage = window.location.pathname.endsWith('index.html') || 
-                  window.location.pathname === '/' || 
-                  window.location.pathname.endsWith('/Store/') ||
-                  window.location.pathname === '';
-
-// Use correct paths based on page location
-const basePath = isMainPage ? 'assets/icons/' : '../assets/icons/';
-
 const instagramImageCache = {
-    'https://www.instagram.com/p/DC9njmdIeNT/': basePath + 'product1.png',
-    'https://www.instagram.com/p/DC9oeeXsIxD/': basePath + 'product2.png',
-    'https://www.instagram.com/p/DDrbMUTohRm/': basePath + 'product3.png',
-    'https://www.instagram.com/p/DE2H4BGqEy9/': basePath + 'product4.png',
-    'https://www.instagram.com/p/DE2IdJ5qub5/': basePath + 'product5.png',
-    'https://www.instagram.com/p/DE2Jy0NKbLi/': basePath + 'product6.png',
-    'https://www.instagram.com/p/DE2KF1RK7yK/': basePath + 'product7.png',
-    'https://www.instagram.com/p/DC9khPuIaql/': basePath + 'product8.png'
+    'https://www.instagram.com/p/DC9njmdIeNT/': '../assets/icons/product1.png',
+    'https://www.instagram.com/p/DC9oeeXsIxD/': '../assets/icons/product2.png',
+    'https://www.instagram.com/p/DDrbMUTohRm/': '../assets/icons/product3.png',
+    'https://www.instagram.com/p/DE2H4BGqEy9/': '../assets/icons/product4.png',
+    'https://www.instagram.com/p/DE2IdJ5qub5/': '../assets/icons/product5.png',
+    'https://www.instagram.com/p/DE2Jy0NKbLi/': '../assets/icons/product6.png',
+    'https://www.instagram.com/p/DE2KF1RK7yK/': '../assets/icons/product7.png',
+    'https://www.instagram.com/p/DC9khPuIaql/': '../assets/icons/product8.png'
 };
 
 /**
@@ -28,7 +19,7 @@ const instagramImageCache = {
  * @param {string} instagramUrl - The Instagram post URL
  * @returns {string|null} - The permanent image URL or null if not found
  */
-function getInstagramImage(instagramUrl) {
+function getInstagramImage(instagramUrl) {q
     return instagramImageCache[instagramUrl] || null;
 }
 
